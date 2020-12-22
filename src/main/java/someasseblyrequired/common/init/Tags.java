e8299516506cf591166479b14d6d44809428e0a7
ochotonida <37985539+ochotonida@users.noreply.github.com>
2020-12-22 16:47:46 +0100

    Tweak tags

--- src/main/java/someasseblyrequired/common/init/Tags.java
+++ src/main/java/someasseblyrequired/common/init/Tags.java
@@ -4,10 +4,10 @@ import net.minecraft.item.Item;
 import net.minecraft.tags.ITag;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.util.ResourceLocation;
-import someasseblyrequired.SomeAssemblyRequired;
+import net.minecraftforge.versions.forge.ForgeVersion;
 
 public class Tags {
 
-    public static final ITag.INamedTag<Item> BREADS = ItemTags.createOptional(new ResourceLocation(SomeAssemblyRequired.MODID, "breads"));
-
+    public static final ITag.INamedTag<Item> BREADS = ItemTags.createOptional(new ResourceLocation(ForgeVersion.MOD_ID, "bread"));
+    public static final ITag.INamedTag<Item> KNIVES = ItemTags.createOptional(new ResourceLocation(ForgeVersion.MOD_ID, "tools/knives"));
 }
