de8dc82a8537148a285d8b9d1cce4e8439be97ba
ochotonida <37985539+ochotonida@users.noreply.github.com>
2020-12-26 17:07:39 +0100

    Tweak bread tag

--- src/main/java/someasseblyrequired/common/init/Tags.java
+++ src/main/java/someasseblyrequired/common/init/Tags.java
@@ -5,9 +5,12 @@ import net.minecraft.tags.ITag;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.versions.forge.ForgeVersion;
+import someasseblyrequired.SomeAssemblyRequired;
 
 public class Tags {
 
-    public static final ITag.INamedTag<Item> BREADS = ItemTags.createOptional(new ResourceLocation(ForgeVersion.MOD_ID, "bread"));
     public static final ITag.INamedTag<Item> KNIVES = ItemTags.createOptional(new ResourceLocation(ForgeVersion.MOD_ID, "tools/knives"));
+    public static final ITag.INamedTag<Item> BREADS = ItemTags.createOptional(new ResourceLocation(SomeAssemblyRequired.MODID, "sandwich_breads"));
+    public static final ITag.INamedTag<Item> TOASTER_METALS = ItemTags.createOptional(new ResourceLocation(SomeAssemblyRequired.MODID, "toaster_metals"));
+    public static final ITag.INamedTag<Item> SMALL_FOODS = ItemTags.createOptional(new ResourceLocation(SomeAssemblyRequired.MODID, "small_foods"));
 }
