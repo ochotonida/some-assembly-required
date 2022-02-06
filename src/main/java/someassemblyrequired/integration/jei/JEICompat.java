ebfe2fda7d8a54dca2b296b2dde12119c550f2f3
ochotonida <37985539+ochotonida@users.noreply.github.com>
2022-02-06 01:22:02 +0100

    Add basic jei category

--- src/main/java/someassemblyrequired/integration/jei/JEICompat.java
+++ src/main/java/someassemblyrequired/integration/jei/JEICompat.java
@@ -2,13 +2,19 @@ package someassemblyrequired.integration.jei;
 
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import mezz.jei.api.registration.ISubtypeRegistration;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import someassemblyrequired.common.init.ModBlocks;
 import someassemblyrequired.common.init.ModItems;
 import someassemblyrequired.common.util.Util;
 import someassemblyrequired.integration.ModCompat;
 import someassemblyrequired.integration.create.CreateCompat;
+
+import java.util.List;
 
 @JeiPlugin
 @SuppressWarnings("unused")
@@ -33,5 +39,16 @@ public class JEICompat implements IModPlugin {
         if (ModCompat.isCreateLoaded()) {
             registration.addRecipes(CreateCompat.createSandwichAssemblingRecipes(), SEQUENCED_ASSEMBLY);
         }
+        registration.addRecipes(List.of(new SandwichAssemblyCategory.Recipe()), SandwichAssemblyCategory.ID);
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        registration.addRecipeCategories(new SandwichAssemblyCategory(registration.getJeiHelpers().getGuiHelper()));
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(ModBlocks.SANDWICHING_STATION.get()), SandwichAssemblyCategory.ID);
     }
 }
